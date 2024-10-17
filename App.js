4913207b7d1ef0ab0e7f38435e29f270763d157c
SampoL <sampojolehtonen@gmail.com>
2024-10-17 16:25:00 +0300

    Navigation icons & Blank BetsScreen added

--- App.js
+++ App.js
@@ -2,9 +2,11 @@ import React, { useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from '@expo/vector-icons/Ionicons';
 import SignInScreen from './screens/SignInScreen';
 import SignOutScreen from './screens/SignOutScreen';
 import HomeScreen from './screens/HomeScreen';
+import BetsScreen from './screens/BetsScreen';
 
 
 const Stack = createStackNavigator();
@@ -12,8 +14,25 @@ const Tab = createBottomTabNavigator();
 
 function MainTabs({ handleSignOut }) {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator 
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === 'Home') {
+            iconName = focused ? 'home' : 'home-outline';
+          } else if (route.name === 'Bets') {
+            iconName = focused ? 'ticket' : 'ticket-outline';
+          } else if (route.name === 'SignOut') {
+            iconName = focused ? 'log-out' : 'log-out-outline';
+          }
+
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+      })}
+    >
       <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Bets" component={BetsScreen} />
       <Tab.Screen name="SignOut">
         {(props) => <SignOutScreen {...props} onSignOut={handleSignOut} />} 
       </Tab.Screen>
---
+++ screens/BetsScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function BetsScreen() {
+    return (
+        <View>
+            <Text>Bets Screen</Text>
+        </View>
+    );
+}
