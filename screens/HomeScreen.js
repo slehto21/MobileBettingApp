fcba6981105fe6148a11400986190c74d58c363b
SampoL <sampojolehtonen@gmail.com>
2024-10-31 13:37:37 +0200

    Line chart using Gifted Charts

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,6 +1,7 @@
-import React, { useContext } from 'react';
+import React, { useContext, useMemo } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { BetsContext } from '../context/BetsContexts';
+import { LineChart } from 'react-native-gifted-charts';
 
 export default function HomeScreen() {
 
@@ -20,6 +21,24 @@ export default function HomeScreen() {
                 return acc;
             }
         }, 0);
+    };
+
+    const calculateBalanceOverTime = () => {
+        // Sort bets by date in ascending order
+        bets.sort((a, b) => a.date - b.date);
+        return bets.reduce((acc, bet) => {
+            console.log('Bet: ', bet);
+            if (bet.status === 'Won') {
+                acc.push({ value: acc[acc.length - 1].value + (bet.stake * (bet.odds - 1)), label: bet.date.toISOString() });
+            } else if (bet.status === 'Lost') {
+                acc.push({ value: acc[acc.length - 1].value - bet.stake, label: bet.date.toISOString() });
+            } else if (bet.status === 'Half Won') {
+                acc.push({ value: acc[acc.length - 1].value + ((bet.stake / 2) * (bet.odds - 1)), label: bet.date.toISOString() });
+            } else if (bet.status === 'Half Lost') {
+                acc.push({ value: acc[acc.length - 1].value - (bet.stake / 2), label: bet.date.toISOString() });
+            }
+            return acc;
+        }, [{ value: 0, label: bets[0].date.toISOString() }]);
     }
 
     const totalBets = bets.length;
@@ -30,43 +49,74 @@ export default function HomeScreen() {
     const avgOdds = bets.reduce((acc, bet) => acc + bet.odds, 0) / totalBets;
     const avgStake = totalStake / totalBets;
     const avgProfit = profit / totalBets;
+    const balanceOverTime = useMemo(() => {
+        if (bets.length === 0 || !bets[0].date) {
+            return [{ value: 0, label: new Date().toISOString() }];
+        }
+        return calculateBalanceOverTime();
+    }, [bets]);
+
+    console.log('Balance Over Time: ', balanceOverTime);
 
     return (
         <View style={styles.container}>
-            <View style={styles.box}>
-                <Text>Total Bets: {totalBets}</Text>
+            <View style={styles.chartContainer}>
+                <LineChart
+                    data={balanceOverTime}
+                    width={300}
+                    height={200}
+                    color="blue"
+                />
+
             </View>
-            <View style={styles.box}>
-                <Text>Total Stake: {totalStake}</Text>
-            </View>
-            <View style={styles.box}>
-                <Text>Profit: {profit}</Text>
-            </View>
-            <View style={styles.box}>
-                <Text>ROI: {roi.toFixed(2)}%</Text>
-            </View>
-            <View style={styles.box}>
-                <Text>Win Rate: {winRate.toFixed(2)}%</Text>
-            </View>
-            <View style={styles.box}>
-                <Text>Avg Odds: {avgOdds.toFixed(2)}</Text>
-            </View>
-            <View style={styles.box}>
-                <Text>Avg Stake: {avgStake.toFixed(2)}</Text>
-            </View>
-            <View style={styles.box}>
-                <Text>Avg Profit: {avgProfit.toFixed(2)}</Text>
+            
+            <View style={styles.statsContainer}>
+                <View style={styles.box}>
+                    <Text>Total Bets: {totalBets}</Text>
+                </View>
+                <View style={styles.box}>
+                    <Text>Total Stake: {totalStake}</Text>
+                </View>
+                <View style={styles.box}>
+                    <Text>Profit: {profit}</Text>
+                </View>
+                <View style={styles.box}>
+                    <Text>ROI: {roi.toFixed(2)}%</Text>
+                </View>
+                <View style={styles.box}>
+                    <Text>Win Rate: {winRate.toFixed(2)}%</Text>
+                </View>
+                <View style={styles.box}>
+                    <Text>Avg Odds: {avgOdds.toFixed(2)}</Text>
+                </View>
+                <View style={styles.box}>
+                    <Text>Avg Stake: {avgStake.toFixed(2)}</Text>
+                </View>
+                <View style={styles.box}>
+                    <Text>Avg Profit: {avgProfit.toFixed(2)}</Text>
+                </View>
             </View>
         </View>
     );
-}
+};
+
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         padding: 10,
     },
+    chartContainer: {
+        alignItems: 'center',
+        marginBottom: 20,
+    },
+    statsContainer: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        justifyContent: 'space-between',
+    },
     box: {
+        width: '48%',
         padding: 10,
         marginVertical: 5,
         backgroundColor: '#f0f0f0',
